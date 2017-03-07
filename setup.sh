fd4f2e97a513beaf11dec5224c95ae7e79de4ac7
Bachelor <bachelor.whc@gmail.com>
2017-03-07 11:36:00 +0800

    update setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ sudo apt install wmctrl -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/
 vim +PluginInstall +qall
+sudo apt install libpython2.7-dev cmake
 pushd .
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
